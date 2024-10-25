e705071b089ba10fe4ec7f80ecdc9deb1b29b32d
SambavJetty <140359924+SambavJetty@users.noreply.github.com>
2024-10-25 21:17:20 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ const LoginSignup = () => {
   const login = async () =>{
     console.log("Login Function Executed",formData);
     let responseData;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://reveria-backend.vercel.app/login',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
@@ -38,7 +38,7 @@ const LoginSignup = () => {
   const signup = async() =>{
     console.log("Signup Function Executed",formData);
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://reveria-backend.vercel.app/signup',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
